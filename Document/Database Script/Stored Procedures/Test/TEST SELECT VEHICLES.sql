99d69161614e5cab7a6d47080751a389d7b80326
Haraldur <haraldur@live.dk>
2023-10-02 08:55:36 +0200

    Update Select Vehicles

--- Document/Database Script/Stored Procedures/Test/TEST SELECT VEHICLES.sql
+++ Document/Database Script/Stored Procedures/Test/TEST SELECT VEHICLES.sql
@@ -3,7 +3,7 @@ GO
 
 DECLARE	@return_value int
 
-EXEC	@return_value = [dbo].[SelectVehical]
+EXEC	@return_value = [dbo].[SelectVehicles]
 		@BaseVehicleId = 11
 
 SELECT	'Return Value' = @return_value
