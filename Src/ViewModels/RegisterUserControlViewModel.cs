63e2dbfcb02f1799d3a1f3d078a3c02bdf883dfb
Renas Shekho <105039988+RenasShekho@users.noreply.github.com>
2023-10-02 11:54:47 +0200

    user controls´s ViewModels has been created, login page has been customized made some design in it.

---
+++ Src/ViewModels/RegisterUserControlViewModel.cs
@@ -0,0 +1,13 @@
+﻿using H2PD071123_Grp2_AutoAuction.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2PD071123_Grp2_AutoAuction.Src.ViewModels
+{
+    internal class RegisterUserControlViewModel:ViewModelBase
+    {
+    }
+}
--- Src/Views/MainWindow.axaml.cs
+++ Src/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using H2PD071123_Grp2_AutoAuction.ViewModels;
 
 namespace H2PD071123_Grp2_AutoAuction.Views;
 
@@ -7,6 +8,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel(); //bind to mainviewmodel
         ContentAreaUserControl.Navigate(new LoginUserControl());
     }
 }
