de3cfa8c2b404dafc0da0d40b67219268bf5e8f7
Haraldur <haraldur@live.dk>
2023-10-03 13:46:19 +0200

    Select All Auction

---
+++ Document/Database Script/Stored Procedures/SelectAllAuction.sql
@@ -0,0 +1,10 @@
+USE auction
+GO
+
+CREATE PROCEDURE SelectAllAuction
+AS
+BEGIN
+    SELECT a.*, b.Name AS BaseVehicleName
+    FROM Auction AS a
+    LEFT JOIN Basevehicles AS b ON a.VehicleId = b.Id;
+END;
